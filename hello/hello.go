e93a6ece32a8fbca255df4f389da65eacaea2e2f
RayKayy <rayk199492@gmail.com>
2021-04-22 23:17:45 -0400

    feat: go basic error handlling

--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
+	"log"
 
 	"example.com/greetings"
+	"rsc.io/quote"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 	fmt.Println(quote.Go())
-	message := greetings.Hello("RayKay")
+	message, err := greetings.Hello("")
+	// If error returned; print to console and exit
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
