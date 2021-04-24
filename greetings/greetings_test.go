0ad9195d835efe754c32d265e10394db48b52ba1
RayKayy <rayk199492@gmail.com>
2021-04-24 17:50:10 -0400

    feat: added tests

---
+++ greetings/greetings_test.go
@@ -0,0 +1,23 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Bob"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello(name)
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Bob") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	name := ""
+	msg, err := Hello(name)
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
